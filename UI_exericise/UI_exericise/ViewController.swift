e9417203b4849e50727f31d2ce3b7203147a97f3
Paweł Brzozowski <mac@192.168.1.5>
2021-11-08 14:12:23 +0100

    UI exericise

---
+++ UI_exericise/UI_exericise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI_exericise
+//
+//  Created by Paweł Brzozowski on 08/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
