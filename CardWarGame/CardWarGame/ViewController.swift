b73d178daa75ef24877d47f48ed50ab87cae43b0
Paweł Brzozowski <mac@MacBook-Air-Pawe.local>
2021-11-09 14:43:35 +0100

    added showing random cards
    when user press deal button,random cards are shown

--- CardWarGame/CardWarGame/ViewController.swift
+++ CardWarGame/CardWarGame/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var leftCard: UIImageView!
+    @IBOutlet weak var rightCard: UIImageView!
+    @IBOutlet weak var playerScore: UILabel!
+    @IBOutlet weak var cpuScore: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func dealButtonPressed(_ sender: UIButton) {
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        leftCard.image = UIImage(named: "card"+String(leftNumber))
+        rightCard.image = UIImage(named: "card"+String(rightNumber))
+    }
+    
 }
 
