46bc30a070c43ec8395d7eeafbaf5345978e80a3
Paweł Brzozowski <mac@192.168.1.5>
2021-11-11 21:32:45 +0100

    added displaying cards
    beta version

--- MatchingPictureApp/MatchingPictureApp/ViewController.swift
+++ MatchingPictureApp/MatchingPictureApp/ViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     let model = CardModel()
     var cardsArray = [Card]()
     
@@ -16,8 +19,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         cardsArray = model.getCards()
+        
+        // THIS NEEDS TO BE SET UP TO INDICATE THAT VEIWCONTROLLER IS DELEGATE
+        // set the view contorller as the deatasourcse and delegate of the collection view
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
-
+    // MARK: - CollectionViewDelegate methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // get a cell as reusable cell to save up memory
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        // TODO: configure cell
+        
+        // return cell
+        return cell
+    }
+    
 }
 
