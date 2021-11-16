83b9ca7e5a34458d60b1cca72c372c1bcf5a4b8d
Paweł Brzozowski <mac@192.168.1.2>
2021-11-16 13:58:55 +0100

    Delegate - Protocol starting project

---
+++ DelegateProtocols/DelegateProtocols/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegateProtocols
+//
+//  Created by Paweł Brzozowski on 16/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
