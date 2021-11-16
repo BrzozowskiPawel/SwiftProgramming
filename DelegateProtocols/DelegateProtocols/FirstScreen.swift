ef8b6372488e94d5e389c640769bc9610beb8070
Paweł Brzozowski <mac@192.168.1.2>
2021-11-16 16:05:18 +0100

    added Delegate-Protocol

--- DelegateProtocols/DelegateProtocols/FirstScreen.swift
+++ DelegateProtocols/DelegateProtocols/FirstScreen.swift
@@ -5,8 +5,15 @@
 //  Created by Paweł Brzozowski on 16/11/2021.
 //
 
+
+// Delegate - Protocol is like master - slave.
+// FirstScreen (just do something) is slave and SecondScreen(know information and send it) is master.
+// Base on picked side (in SecondScreen) we are gona pass some data back to the FirstScreen
+// When FirstScreen gets data it does what code says
+
 import UIKit
 
+// This screen just gets the information and show it.
 class FirstScreen: UIViewController {
 
     @IBOutlet weak var mainImageView: UIImageView!
@@ -24,17 +31,23 @@ class FirstScreen: UIViewController {
     // When user press this button, screen is changed
     @IBAction func chooseButtonTapped(_ sender: UIButton) {
         let secondScreenVC = storyboard?.instantiateViewController(withIdentifier: "SecondScreen") as! SecondScreen
+        
+        // 4. Setting SecondScreen var as this class (This class want to by slave for master).
+        secondScreenVC.selectionDelegate = self
         present(secondScreenVC, animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+// 5. Creating extension for this ViewController to fulfill Protocol requariments
+extension FirstScreen: SideSelectionDelegate {
+    // 6. This function will be called when SecondScreen (master) calls this funtion.
+    //    In thic ViewController this inst being called by user.
+    func didTapChoice(image: UIImage, name: String, color: UIColor) {
+        mainImageView.image = image
+        nameLabel.text = name
+        view.backgroundColor = color
     }
-    */
-
+    
+    
 }
--- DelegateProtocols/DelegateProtocols/SecondScreen.swift
+++ DelegateProtocols/DelegateProtocols/SecondScreen.swift
@@ -5,24 +5,43 @@
 //  Created by Paweł Brzozowski on 16/11/2021.
 //
 
+// Delegate - Protocol is like master - slave.
+// FirstScreen (just do something) is slave and SecondScreen(know information and send it) is master.
+// Base on picked side (in SecondScreen) we are gona pass some data back to the FirstScreen
+// When FirstScreen gets data it does what code says
+
 import UIKit
+
+// 1. Setting up Protocol (on master side)
+protocol SideSelectionDelegate {
+    // Just a list of function WITHOUT CODE (list of commands)
+    func didTapChoice(image: UIImage, name: String, color: UIColor)
+}
 
 class SecondScreen: UIViewController {
 
+    // 2. Creating need to have variable to hold delegate
+    var selectionDelegate: SideSelectionDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
     @IBAction func whiteVuttonTapped(_ sender: UIButton) {
         // Imperial
+        
+        // 3. Declaring function as it need to be declared becase of the protocol
+        selectionDelegate.didTapChoice(image: UIImage(named: "vader")!, name: "Darth Vader", color: .red)
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func redButtonTapped(_ sender: UIButton) {
         // Rebel
+        
+        // 3. Declaring function as it need to be declared becase of the protocol
+        selectionDelegate.didTapChoice(image: UIImage(named: "luke")!, name: "Luke Skywalker", color: .cyan)
         dismiss(animated: true, completion: nil)
     }
     
