e928be4df9c4997559e440de8a4431449b3fb28f
Paweł Brzozowski <mac@192.168.1.3>
2021-11-24 17:35:53 +0100

    more Firebase baciscs

--- Firestore Demo/Firestore Demo/ViewController.swift
+++ Firestore Demo/Firestore Demo/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
         //db.collection("users").document("userid123").setData(["firstname":"Tom","lastname":"Bee","age":25])
         
         // Adding a document with auto ID
-        //db.collection("users").addDocument(data: ["firstname":"Bob","lastname":"Bobowsky","age":100])
+        // db.collection("users").addDocument(data: ["firstname":"John","lastname":"krasinki","age":40])
         
         // Adding a docu,mnet woth a utogenerted id while getting referne to the id
 //        let myNewDocument = db.collection("users").document()
@@ -46,9 +46,90 @@ class ViewController: UIViewController {
         // If this exist it will udpate and if not it will add
         // db.collection("users").document("userid123").updateData(["test":"test"])
         
-        // MARK: Delating data
+        // MARK: - Delating data
         
         // deliting a documnet
+        // db.collection("users").document("a0STv31plRwHdnxv76VM").delete()
+        
+        // deliting a specyfic field
+        // db.collection("users").document("userid123").updateData(["test":FieldValue.delete()])
+        
+        // MARK: - Handling errors
+        
+        // All methods have compliton - you can specyfic what will be done in complition
+//        db.collection("users").addDocument(data: [:]) { error in
+//            if error != nil {
+//                print(error!)
+//            }
+//        }
+        
+        // MARK: - Reading data
+        
+        // reradin a single document
+//        db.collection("users").document("userid123").getDocument { docSpanshot, error in
+//            if error == nil && docSpanshot != nil && docSpanshot!.data() != nil{
+//                // No error, we have snapshot and data
+//                print(docSpanshot!.data()!)
+//            } else {
+//                print("Error while reading document: \(error)")
+//            }
+//        }
+        
+        // Reading a collection of documents
+//        db.collection("users").getDocuments { querySpanshots, error in
+//            if error == nil && querySpanshots != nil {
+//            // No error, we have snapshot and da
+//                for doc in querySpanshots!.documents{
+//                    print(doc.documentID)
+//                }
+//            } else {
+//                print("Error while reading document: \(error!)")
+//            }
+//        }
+        
+        // MARK: - Listening for data
+        
+        // Lstening in changes (single document)
+//        let listener = db.collection("users").document("userid123").addSnapshotListener { docSnapshot, error in
+//            if error == nil && docSnapshot != nil && docSnapshot!.data() != nil{
+//                // No error, we have snapshot and data
+//                print(docSnapshot!.data()!)
+//            } else {
+//                print("Error while reading document: \(error!)")
+//            }
+//
+//        }
+//
+        // Remeber to remove the listener
+        // listener.remove()
+        
+        // Listening for changes in a collection
+//        let listener = db.collection("users").addSnapshotListener { querySnapshot, error in
+//            // chech for errors
+//            if error == nil && querySnapshot != nil {
+//                // Reading only changes
+//                for change in querySnapshot!.documentChanges {
+//                    print(change.document.documentID)
+//                }
+//            }
+//
+//        }
+        
+        // Remeber to remove the listener
+        // listener.remove()
+        
+        // MARK: - Quering data from FireBase
+        // Example returnig everybody that have age = 20.
+        
+        // This return a querry -> need to add .getDocument or add a listener
+//        db.collection("users").whereField("age", isEqualTo: 20).getDocuments { snapShot, error in
+//            let data =  snapShot!.documents
+//
+//            for doc in data {
+//                print(doc.documentID)
+//            }
+//        }
+        
     }
     
 
