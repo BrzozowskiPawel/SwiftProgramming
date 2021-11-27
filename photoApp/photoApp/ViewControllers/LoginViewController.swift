f760e2a3974dffa1789b58b60a518ae6396402d0
Paweł Brzozowski <mac@Pawes-MacBook-Air.local>
2021-11-27 14:38:46 +0100

    added FirebaseAuthUI

--- photoApp/photoApp/ViewControllers/LoginViewController.swift
+++ photoApp/photoApp/ViewControllers/LoginViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuthUI
+import FirebaseEmailAuthUI
 
 class LoginViewController: UIViewController {
 
@@ -17,7 +19,43 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginButtonTapped(_ sender: Any) {
+        
+        // Create a FirebaseAuthUI object
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        // chech that it's not nil
+        if let authUI = authUI {
+            // Set self as delgate for the authUI
+            authUI.delegate = self
+            
+            // Set sign in providers
+            authUI.providers = [FUIEmailAuth()]
+            
+            // Get the prebuilt UI view controller
+            let authViewController = authUI.authViewController()
+            
+            // Present it
+            present(authViewController, animated: true, completion: nil)
+        }
     }
-    
+}
 
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        if error != nil {
+            // There was an error
+            print("error \(error!)")
+            return
+        }
+        
+        // Geting ad checking for nil user
+        let user =  authDataResult?.user
+        if let user = user {
+            // chech on the database side if user has a profile
+            
+            // if user dont have a profile create a profile view contorller
+            
+            // if have password, go to tab bar controller
+        }
+    }
 }
