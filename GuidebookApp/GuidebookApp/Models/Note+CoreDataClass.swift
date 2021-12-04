ead416c71c428a73114e9ffdfdf55131a2eca715
Paweł Brzozowski <mac@192.168.1.3>
2021-12-04 10:06:10 +0100

    added CoreData models

---
+++ GuidebookApp/GuidebookApp/Models/Note+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Note+CoreDataClass.swift
+//  GuidebookApp
+//
+//  Created by Paweł Brzozowski on 04/12/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Note)
+public class Note: NSManagedObject {
+
+}
---
+++ GuidebookApp/GuidebookApp/Models/Place+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Place+CoreDataClass.swift
+//  GuidebookApp
+//
+//  Created by Paweł Brzozowski on 04/12/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Place)
+public class Place: NSManagedObject {
+
+}
