c62596b6bd71834d278fc231dfcb10b55f3ae881
Paweł Brzozowski <mac@192.168.1.3>
2021-12-07 11:34:04 +0100

    UI view of App

---
+++ GuideBook/GuideBook/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  GuideBook
+//
+//  Created by Paweł Brzozowski on 07/12/2021.
+//
+
+import Foundation
+
+struct Constants {
+    static let SEGUE_DETAIL = "goToDetail"
+    static let NOTESVC_STORYBOARDID = "NotesViewController"
+    static let COMPOSE_STORYBOARDID = "ComposeViewController"
+    static let MAP_STORYBOARDID = "MapViewController"
+}
