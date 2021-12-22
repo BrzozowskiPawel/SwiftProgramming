cab25fdcfe9db26802be2a62da5cff7c3fa24790
Paweł Brzozowski <mac@Pawes-MacBook-Air.local>
2021-12-22 14:59:29 +0100

    added programatic UI

---
+++ YouTube/YouTube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by Paweł Brzozowski on 16/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
