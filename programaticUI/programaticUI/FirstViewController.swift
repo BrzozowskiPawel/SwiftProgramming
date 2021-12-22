2d81897225397f9b3a3d4661699bd277dd49dacb
Paweł Brzozowski <mac@Pawes-MacBook-Air.local>
2021-12-22 18:36:30 +0100

    setup UI only with code

--- programaticUI/programaticUI/FirstViewController.swift
+++ programaticUI/programaticUI/FirstViewController.swift
@@ -9,14 +9,126 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    var contentView: UIView = UIView()
+    var background: UIImageView = UIImageView()
+    var fbButton: UIButton = UIButton()
+    var gmailButton: UIButton = UIButton()
+    var emailButton: UIButton = UIButton()
+    var titleLabel: UILabel = UILabel()
+    var hintLabel: UILabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = UIColor.red
+        
+        self.view.addSubview(contentView)
+        
+        setUpContent()
     }
     
 
+    // set up the content
+    func setUpContent() {
+        contentView.addSubview(background)
+        contentView.addSubview(fbButton)
+        contentView.addSubview(gmailButton)
+        contentView.addSubview(emailButton)
+        contentView.addSubview(hintLabel)
+        contentView.addSubview(titleLabel)
+        
+        // In order to utilize auto layout we will need to "activate" it by setting
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Settingup constraints
+        contentView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        contentView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        
+        setUpBackground()
+        setUpFbButton()
+        setUpEmailButton()
+        setUpGmailButton()
+        setUpHintLabel()
+        setUpTitleLabel()
+    }
+    
+    func setUpBackground(){
+        
+        background.image = UIImage(named: "Background")
+        
+        background.translatesAutoresizingMaskIntoConstraints = false
+        
+        background.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        background.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+        background.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        background.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    }
+
+    func setUpFbButton(){
+        
+        fbButton.setBackgroundImage(UIImage(named: "FB"), for: .normal)
+        fbButton.setTitle("Sign in with Facebook", for: .normal)
+        fbButton.setTitleColor(.black, for: .normal)
+        
+        fbButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        fbButton.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 50).isActive = true
+        fbButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -50).isActive = true
+        fbButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        fbButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+    }
+
+    func setUpEmailButton(){
+        gmailButton.setBackgroundImage(UIImage(named: "Google"), for: .normal)
+        gmailButton.setTitle("Sign in with Google", for: .normal)
+        gmailButton.setTitleColor(.black, for: .normal)
+        
+        gmailButton.translatesAutoresizingMaskIntoConstraints = false
+
+        gmailButton.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 50).isActive = true
+        gmailButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -50).isActive = true
+        gmailButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        gmailButton.topAnchor.constraint(equalTo: fbButton.bottomAnchor,constant: 20).isActive = true
+    }
+
+    func setUpGmailButton(){
+        emailButton.setBackgroundImage(UIImage(named: "Email"), for: .normal)
+        emailButton.setTitle("Sign in with Email", for: .normal)
+        
+        emailButton.setTitleColor(.black, for: .normal)
+        
+        emailButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        emailButton.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 50).isActive = true
+        emailButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -50).isActive = true
+        emailButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        emailButton.topAnchor.constraint(equalTo: gmailButton.bottomAnchor,constant: 20).isActive = true
+    }
+
+    func setUpHintLabel(){
+        
+        hintLabel.text = "Login or create an account"
+        
+        hintLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        hintLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        hintLabel.bottomAnchor.constraint(equalTo: fbButton.topAnchor, constant: -20).isActive = true
+    }
+
+    func setUpTitleLabel(){
+        titleLabel.text = "Social \nSquid"
+        titleLabel.numberOfLines = 0
+        
+        titleLabel.font = UIFont(name: "AvenirNext-Regular", size: 44)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        titleLabel.bottomAnchor.constraint(equalTo: hintLabel.topAnchor, constant: -50).isActive = true
+    }
+    
     /*
     // MARK: - Navigation
 
