b36f00d30c915ef3681e40ce340f0109f3ed01ef
Paweł Brzozowski <mac@Pawes-MacBook-Air.local>
2021-12-22 16:04:34 +0100

    succesfully setup new VC and used it as root

---
+++ programaticUI/programaticUI/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  programaticUI
+//
+//  Created by Paweł Brzozowski on 22/12/2021.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
