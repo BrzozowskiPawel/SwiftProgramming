f7ddbd6cc4cc595288e93f89ee00d81c02b87780
Paweł Brzozowski <mac@Pawes-MacBook-Air.local>
2021-12-30 14:00:33 +0100

    UI for CABasicAnimation

---
+++ Animations/Animations/Components/AnimatedLabel.swift
@@ -0,0 +1,20 @@
+//
+//  AnimatedLabel.swift
+//  Animations
+//
+//  Created by Paweł Brzozowski on 30/12/2021.
+//
+
+import UIKit
+
+class AnimatedLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Animations/Animations/ViewController.swift
+++ Animations/Animations/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var animationSegmentedController: UISegmentedControl!
+    @IBOutlet weak var animationTypeLabel: UILabel!
+    @IBOutlet weak var animatedButton: AnimatedButton!
+    @IBOutlet weak var animatedLabel: AnimatedLabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func animationSegmentedController(_ sender: Any) {
+        switch animationSegmentedController.selectedSegmentIndex
+            {
+            case 0:
+            animatedLabel.text = "1"
+            case 1:
+            animatedLabel.text = "2"
+            default:
+                break
+            }
+    }
+    
+    
 }
 
