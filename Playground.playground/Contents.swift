776b659559a5926e5e2237d565b5c80bfb31b57c
Paweł Brzozowski <mac@Pawes-MacBook-Air.local>
2021-12-31 13:54:17 +0100

    computed properties

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+// Mark: - Computed properties
+
+let pizzaInches: Int = 14
+
+var numberOfSlices: Int {
+    return pizzaInches - 4
+}
+
+print(numberOfSlices)
