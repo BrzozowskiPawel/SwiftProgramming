6c31d703930f0082c33786b5dedd878b6b857c24
Paweł Brzozowski <mac@Pawes-MacBook-Air.local>
2021-12-31 14:05:23 +0100

    Computed properties changes

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,13 +1,19 @@
-import UIKit
-
-var greeting = "Hello, playground"
+//import UIKit - doesnt exist for MacOS
+import Foundation
 
 // Mark: - Computed properties
 
 let pizzaInches: Int = 14
 
 var numberOfSlices: Int {
-    return pizzaInches - 4
+    get {
+        return pizzaInches - 4
+    }
+    // Called when there is change in numberOfSlices
+    // Without it programer cannot set new val for this propery (it can be used tho).
+    set {
+        print("Number of slices now has a new val: \(newValue)")
+    }
 }
 
-print(numberOfSlices)
+numberOfSlices = 12
