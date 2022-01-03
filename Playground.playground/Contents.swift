49f325e9d61b79af04ff6e2548d7831bdb4b3ad0
Paweł Brzozowski <mac@192.168.1.2>
2022-01-03 19:45:04 +0100

    added ML hotdog/notAHotDog

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -37,49 +37,77 @@ import Foundation
 
 // Ex. Somebody want to buy 33" pizza which is to big value... (at least for normal human XD)
 
-var pizzaInches: Int = 10 {
-    // both willSet and didSet monitor if pizzaInches changed
-    willSet {
-        // Will trigger right before val is change
-        print("new value of pizzaInches set to: \(newValue)") // Acces to new value
-    }
-    didSet {
-        // Right after it changes this will execute
-        print("previous value of pizzaInches set to: \(oldValue)") // Acces to old val (setted before)
-        if pizzaInches >= 18 {
-            print("SORRY BUT THIS PIZZA IS TOOOOOO BIG 😳 - setting piza to max (18 inch)")
-            pizzaInches = 18
-        }
-    }
-}
+//var pizzaInches: Int = 10 {
+//    // both willSet and didSet monitor if pizzaInches changed
+//    willSet {
+//        // Will trigger right before val is change
+//        print("new value of pizzaInches set to: \(newValue)") // Acces to new value
+//    }
+//    didSet {
+//        // Right after it changes this will execute
+//        print("previous value of pizzaInches set to: \(oldValue)") // Acces to old val (setted before)
+//        if pizzaInches >= 18 {
+//            print("SORRY BUT THIS PIZZA IS TOOOOOO BIG 😳 - setting piza to max (18 inch)")
+//            pizzaInches = 18
+//        }
+//    }
+//}
+//
+//pizzaInches = 20
+//
+//var numberOFPeople: Int = 6
+//let slicesPerPerson: Int = 5
+//
+//var numberOfSlices: Int {
+//    get {
+//        return pizzaInches - 4
+//    }
+//    // Called when there is change in numberOfSlices
+//    // Without it programer cannot set new val for this propery (it can be used tho).
+//    set {
+//        print("Number of slices now has a new val: \(newValue)")
+//    }
+//}
+//
+//
+//// Calculate number of pizzas acording to dominos
+//var numberOfPizzas: Int {
+//    get {
+//        let numberOfPeopleFedPerPizza = numberOfSlices / slicesPerPerson
+//        return numberOFPeople / numberOfPeopleFedPerPizza
+//    } set {
+//        let totalSlices = numberOfSlices * newValue // newValue is new value of numberOfPizzas
+//        numberOFPeople = totalSlices / slicesPerPerson
+//    }
+//}
+//
+//numberOfPizzas = 4
+//print(numberOfPizzas)
 
-pizzaInches = 20
+// MARK: - Computed + Observed properties task
 
-var numberOFPeople: Int = 6
-let slicesPerPerson: Int = 5
+// Calc number of bucket to paint this wall.
+// Single bucket only coves 1.5 sqm
 
-var numberOfSlices: Int {
+var width: Float = 1.5
+var heiht: Float = 2.3
+
+var bucketsOfPaint: Int {
     get {
-        return pizzaInches - 4
+        let area = width * heiht
+        let coverPerBucket: Float = 1.5
+        return Int(ceilf(area / coverPerBucket))
     }
-    // Called when there is change in numberOfSlices
-    // Without it programer cannot set new val for this propery (it can be used tho).
     set {
-        print("Number of slices now has a new val: \(newValue)")
+        let areaToBeCovered = Double(newValue) * 1.5
+        print("This is enough for \(areaToBeCovered)")
     }
 }
 
+print(bucketsOfPaint)
 
-// Calculate number of pizzas acording to dominos
-var numberOfPizzas: Int {
-    get {
-        let numberOfPeopleFedPerPizza = numberOfSlices / slicesPerPerson
-        return numberOFPeople / numberOfPeopleFedPerPizza
-    } set {
-        let totalSlices = numberOfSlices * newValue // newValue is new value of numberOfPizzas
-        numberOFPeople = totalSlices / slicesPerPerson
-    }
-}
+bucketsOfPaint = 10
 
-numberOfPizzas = 4
-print(numberOfPizzas)
+// How many sqm we can covers if there is a few buckets left
+
+
