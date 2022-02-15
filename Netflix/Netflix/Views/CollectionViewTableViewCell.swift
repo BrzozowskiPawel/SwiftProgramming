b5a3598d8f5b8837decd2226de814a0155ce909a
Paweł Brzozowski <mac@192.168.1.6>
2022-02-15 20:54:26 +0100

    working on tableView/cells

---
+++ Netflix/Netflix/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix
+//
+//  Created by Paweł Brzozowski on 15/02/2022.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
