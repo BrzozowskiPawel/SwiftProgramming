32b7de93a02f7d770664c935743bc2780f9290d8
Paweł Brzozowski <mac@192.168.1.6>
2022-02-16 21:31:11 +0100

    added play button for hero header

--- Netflix/Netflix/Views/HeroHeaderUIView.swift
+++ Netflix/Netflix/Views/HeroHeaderUIView.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
 
+    private let playButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.systemBackground.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
     private let heroImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -17,9 +27,40 @@ class HeroHeaderUIView: UIView {
         return imageView
     }()
     
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        // Add gradient to the View
+        layer.addSublayer(gradientLayer)
+    }
+    
+    private func applyConstraints() {
+        let playButtonConstraintes = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20)
+        ]
+        
+        NSLayoutConstraint.activate(playButtonConstraintes)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        // Add image to the View
         addSubview(heroImageView)
+        
+        // Apply gradient
+        addGradient()
+        
+        // Add play button
+        addSubview(playButton)
+        
+        // Apply constarints
+        applyConstraints()
     }
     
     override func layoutSubviews() {
