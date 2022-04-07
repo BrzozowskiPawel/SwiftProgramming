c79c142a055692280b75f666f4ce44f2eef1be38
PawelB <pawel.brzozowski@appunite.com>
2022-04-07 20:29:55 +0200

    PhotoMemory finished

--- PhotoMemory/PhotoMemory/ViewController.swift
+++ PhotoMemory/PhotoMemory/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -26,6 +26,15 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCollectionViewCell else {
             fatalError("Unable to deques PersonCell")
         }
+        
+        let person = people[indexPath.item]
+        cell.name.text = person.name
+        let path = getDocumentDirectory().appendingPathComponent(person.imgae)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
         
         return cell
     }
@@ -49,6 +58,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         let person = Person(name: "Unknown", image: imageName)
         people.append(person)
+        collectionView.reloadData()
         
         dismiss(animated: true)
     }
@@ -56,6 +66,23 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     func getDocumentDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+        
+        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self, weak ac] action in
+            guard let newName = ac?.textFields?[0].text else {return}
+            person.name = newName
+            self?.collectionView.reloadData()
+        }))
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(ac, animated: true)
     }
 }
 
